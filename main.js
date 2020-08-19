2788e0f42f77c86b90aa625743b49f04bd0d39c0
Appurist / Paul <dev@appurist.com>
2020-08-19 03:15:37 -0300

    db.init is async, and other cleanup.

--- main.js
+++ main.js
@@ -9,69 +9,73 @@ if (config.error) {
   return 1
 } 
 
-let keypath = path.resolve(__dirname, 'ssl', 'server.key');
-let crtpath = path.resolve(__dirname,'ssl', 'server.crt');
+// Dump the config to the startup messages.
+console.log(`Server branding: '${config.NAME}' (${config.STATUS_NAME}) at ${config.DOMAIN}`);
+console.log('New user registration:', config.ALLOW_REGISTER ? 'allowed' : 'disabled');
+// console.log(`Data storage: ${config.DATA}`);
 
-let options = { logger: false };
-let sslOptions = undefined;
-
-if (fs.existsSync(keypath) && fs.existsSync(crtpath)) {
-  let sslkey = fs.readFileSync(keypath);
-  let sslcrt = fs.readFileSync(crtpath);
-
-  sslOptions = {
-    logger: false,
-    http2: true,
-    https: {
-      allowHTTP1: true, // fallback support for HTTP1
-      key: sslkey,
-      cert: sslcrt
-    }
-  }
-  console.log("Enabled HTTPS via SSL certificate files.");
-} else {
-  console.warn("HTTPS disabled: no SSL certificate files provided.")
-}
-
-const fastify = require('fastify')(sslOptions || options);
-
-fastify.register(require('fastify-websocket'));
-
-// Initialize the connection to the Fauna db
 const db = require('./db')
-db.init();
 
-// Deal with CORS by enabling it since this is an API for all.
-fastify.register(require('fastify-cors'), { });
-
-// fastify.options('*', (request, reply) => { reply.send() })
-
-fastify.setErrorHandler(function (error, request, reply) {
-  // Send error response
-  console.warn("fastify error handler for ",error);
-  let code = 500;
-  let message = 'Unknown server error';
-  if (error.statusCode)
-    code = error.statusCode;
-  else
-  if (error.message)
-    message = error.message;
-  reply.code(code).send(message);
-})
-
-// Initialize the Fastify REST API endpoints.
-routes.init(fastify);
-// Start the server listening.
-fastify.listen(config.PORT, config.HOST, (err) => {
-  if (err) {
-    console.error(err.message);
-    process.exit(1)
+// db.init is async. We don't really need to wait,
+// but the messages come out in order then, so lets.
+db.init().then(()=> {
+  let keypath = path.resolve(__dirname, 'ssl', 'server.key');
+  let crtpath = path.resolve(__dirname,'ssl', 'server.crt');
+  
+  let options = { logger: false };
+  let sslOptions = undefined;
+  
+  if (fs.existsSync(keypath) && fs.existsSync(crtpath)) {
+    let sslkey = fs.readFileSync(keypath);
+    let sslcrt = fs.readFileSync(crtpath);
+  
+    sslOptions = {
+      logger: false,
+      http2: true,
+      https: {
+        allowHTTP1: true, // fallback support for HTTP1
+        key: sslkey,
+        cert: sslcrt
+      }
+    }
+    console.log("Enabled HTTPS via SSL certificate files.");
+  } else {
+    console.warn("HTTPS disabled: SSL certificate files NOT provided.")
   }
+  
+  const fastify = require('fastify')(sslOptions || options);
+  fastify.register(require('fastify-websocket'));
+  // Deal with CORS by enabling it since this is an API for all.
+  fastify.register(require('fastify-cors'), { });
+  // fastify.options('*', (request, reply) => { reply.send() })
 
-  let port = fastify.server.address().port;
-  console.log(`Server listening on port ${port}.`);
-})
+  fastify.setErrorHandler(function (error, request, reply) {
+    // Send error response
+    console.warn("fastify error handler for ",error);
+    let code = 500;
+    let message = 'Unknown server error';
+    if (error.statusCode)
+      code = error.statusCode;
+    else
+    if (error.message)
+      message = error.message;
+    reply.code(code).send(message);
+  })
 
-fastify.ready(() => {
-  // console.log(fastify.printRoutes())
-})
+  // Initialize the Fastify REST API endpoints.
+  routes.init(fastify);
+  // Start the server listening.
+  fastify.listen(config.PORT, config.HOST, (err) => {
+    if (err) {
+      console.error(err.message);
+      process.exit(1)
+    }
+
+    let port = fastify.server.address().port;
+    console.log(`Server listening on port ${port}.`);
+  })
+
+  fastify.ready(() => {
+    // console.log(fastify.printRoutes())
+  })
+});
