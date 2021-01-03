bc102676139a37bc140e5df3e9042596d9973e65
Appurist / Paul <dev@appurist.com>
2021-01-02 21:13:22 -0400

    A bit of cleanup of unused items.

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,6 @@
 module.exports.SERVER_CFG = 'sossbox.cfg';
-module.exports.SITE_CFG = 'site.cfg';
 module.exports.USERMETA = 'meta.json';
-
 module.exports.PUBLIC_FOLDER = 'public'
-module.exports.SITES_FOLDER = 'sites';
 module.exports.DATA_FOLDER = 'data';
-
 module.exports.KEY_FILE = 'server.key'
 module.exports.CRT_FILE = 'server.crt'
-
-
