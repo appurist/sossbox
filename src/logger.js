764d8623231ccba55d0444b80be4824e40ca2381
Appurist / Paul <dev@appurist.com>
2021-04-19 03:22:30 -0300

    Okay this didn't help. Open still async so it opens it multiple times.

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const fsp = require('fs').promises;
+const fs = require('fs');
+const fsp = fs.promises;
 
 const OFF = 0;
 const DEBUG = 1;
@@ -16,7 +17,7 @@ function rootname(fn) {
   return path.basename(path.basename(fn, '.exe'), '.EXE');
 }
 
-function execFolder() {
+function executableFolder() {
   let arg = 0;
   let root = rootname(process.argv[0]);
   if (root.toLowerCase() === 'node') {
@@ -26,14 +27,32 @@ function execFolder() {
   return path.dirname(process.argv[arg]);
 }
 
-let folder = execFolder();
-
-let logPath = path.join(folder,'sossbox.log');
+let execFolder = executableFolder();
+let logPath = path.join(execFolder,'sossbox.log');
 let logFile = null;
 let logLevel = WARN;
 
+init(logLevel, 'sossbox.log');
+
 // loglevel must be a normal loglevel string from the levels array above.
-function init(level, fn) {
+async function init(level, fn) {
+  try {
+    setLevel(level);
+
+    let newLogPath = path.isAbsolute(fn) ? path.resolve(fn) : path.resolve(execFolder, fn);
+    if (logFile && (newLogPath !== logPath)) {
+      await logFile.close();
+      logFile = null;
+    }
+    if (!logFile) {
+      await fsp.open(newLogPath, 'w');
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+function setLevel(level) {
   if (typeof level === 'string') {
     switch (level) {
       case 'debug': logLevel = DEBUG; break;
@@ -46,32 +65,24 @@ function init(level, fn) {
   } else {
     logLevel = level;
   }
-
-  if (fn) logPath = path.join(process.cwd(), fn);
-}
-
-function setLevel(level) {
-  logLevel = level;
-}
-
-function open() {
-  if(logFile)
-    return Promise.resolve(logFile);
-  return fsp.open(logPath, 'w');
 }
 
 function log(level, msg) {
-  if (level < logLevel) {
-    return; // below the reporting threshold, nothing to do.
-  }
+  try {
+    if (level < logLevel) {
+      return; // below the reporting threshold, nothing to do.
+    }
 
-  open().then((f) => {
-    if (!logFile) logFile = f;
     console.log(msg);
-    fsp.writeFile(f, msg).then().catch((err)=>{
+    logFile.appendFile(msg)
+    .then()
+    .catch((err)=>{
       console.error(err.message);
     });
-  }).catch((err)=>{console.error(err.message)});
+  }
+  catch(err) {
+    console.error(err.message);
+  }
 }
 
 function debug(msg) { log(DEBUG, msg); }
@@ -80,4 +91,4 @@ function warn(msg) { log(WARN, msg); }
 function error(msg) { log(ERROR, msg); }
 function fatal(msg) { log(FATAL, msg); }
 
-module.exports = { OFF, DEBUG, INFO, WARN, ERROR, FATAL, init, open, log, debug, info, warn, error, fatal};
+module.exports = { OFF, DEBUG, INFO, WARN, ERROR, FATAL, init, log, debug, info, warn, error, fatal};
